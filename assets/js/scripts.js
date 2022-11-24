652083692d4ab93faf86f0266ed5d4f21959eee7
Jhonata Souza <bsi.jcsouza@gmail.com>
2022-11-24 19:09:21 -0300

    Aula sobre JS

---
+++ assets/js/scripts.js
@@ -0,0 +1,21 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+document.getElementById('subtrair').addEventListener("click", function decrement(){
+    if(currentNumber==0){
+        document.getElementById("currentNumber").style.color = "red";
+    }else{
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+        document.getElementById("currentNumber").style.color = "black";
+    }
+});
+document.getElementById('adicionar').addEventListener("click", function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    document.getElementById("currentNumber").style.color = "black";
+});
+
+
+
+
